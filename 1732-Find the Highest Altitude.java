f8940b488cbae99c9768fcc0d934855b95709de2
Sahil Patil <sahilpatil0922@gmail.com>
2022-01-06 13:04:20 +0530

    Create 1732-Find the Highest Altitude.java

---
+++ 1732-Find the Highest Altitude.java
@@ -0,0 +1,15 @@
+public int largestAltitude(int[] gain) {
+        int[] altitude = new int[gain.length + 1];
+        
+        altitude[0] = 0;
+        for(int i = 0; i<gain.length; i++){
+            altitude[i + 1] = altitude[i] + gain[i];
+        }
+        
+        int max = altitude[0];
+        for(int i = 1; i<altitude.length; i++){
+           max = Math.max(max, altitude[i]);
+        }
+        
+        return max;
+    }
