5698673d0b040c1d14ec20f428666976345d23d5
Sahil Patil <sahilpatil0922@gmail.com>
2022-01-10 13:51:33 +0530

    Create 589-N-ary Tree Preorder Traversal.java

---
+++ 589-N-ary Tree Preorder Traversal.java
@@ -0,0 +1,16 @@
+public void preorderHelper(Node root, List<Integer> list){
+        if(root == null) return;
+        
+        list.add(root.val);
+        
+        for(Node child : root.children){
+            preorderHelper(child, list);
+        }
+    }
+    
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        
+        preorderHelper(root,list);
+        return list;
+    }
