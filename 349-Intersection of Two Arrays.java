8bffbaf1f46406803f8039b42f2693682ef6337b
Sahil Patil <sahilpatil0922@gmail.com>
2022-01-25 15:58:21 +0530

    Update 349-Intersection of Two Arrays.java

--- 349-Intersection of Two Arrays.java
+++ 349-Intersection of Two Arrays.java
@@ -1,5 +1,36 @@
-//its O(N^2) worst case approach using 2 for loops and will upload the optimise appraoch later
+//best approach O(N) using freq array to store
+public int[] intersection(int[] nums1, int[] nums2) {
+        
+        //since size of nums1 and nums2 is 1000
+        
+        int[] freq = new int[1001];
+        
+        for(int val : nums1){
+            freq[val]++;
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int val : nums2){
+            if(freq[val] > 0){
+                list.add(val);
+                freq[val] = 0;
+            }
+        }
+        
+        int[] res = new int[list.size()];
+        
+        int i = 0;
+        for(int val : list){
+            res[i++] = val;
+        }
+        
+        return res;
+    }
 
+
+
+//its O(N^2) worst case approach using 2 for loops 
 public int[] intersection(int[] nums1, int[] nums2) {
         List<Integer> list = new ArrayList<>();
         
