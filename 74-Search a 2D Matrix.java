5fddd2845a7686a01bcbf5357413cd54d87c809c
Sahil Patil <sahilpatil0922@gmail.com>
2022-01-26 11:52:57 +0530

    Create 74-Search a 2D Matrix.java

---
+++ 74-Search a 2D Matrix.java
@@ -0,0 +1,22 @@
+// optimal solution like binary search in 1D array
+
+public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int i = 0;
+        int j = n - 1;
+        
+        while(i<m && j>=0){
+            if(matrix[i][j] < target){
+                i++;
+            }else if(matrix[i][j] > target){
+                j--;
+            }else{
+                return true;
+            }
+        }
+        
+        return false;
+    }
