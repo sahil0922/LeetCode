ccb99f2ef1bd74f61e200fe736ba11cd5e95fd18
Sahil Patil <sahilpatil0922@gmail.com>
2022-01-28 01:20:33 +0530

    Update 383-Ransom Note.java

--- 383-Ransom Note.java
+++ 383-Ransom Note.java
@@ -1,3 +1,46 @@
+//O(N) time and O(1) space
+
+public boolean canConstruct(String ransomNote, String magazine) {
+        int[] ransomChar = new int[26];
+        int[] magazineChar = new int[26];
+        
+        for(int i = 0; i<ransomNote.length(); i++){
+            char ch = ransomNote.charAt(i);
+            
+            ransomChar[ch - 'a']++;
+        }
+        
+        for(int i = 0; i<magazine.length(); i++){
+            char ch = magazine.charAt(i);
+            
+            magazineChar[ch - 'a']++;
+        }
+        
+        //find length of non zero ransomchar elements
+        int anslen = 0;
+        for(int val : ransomChar){
+            if(val != 0){
+                anslen++;
+            }
+        }
+        
+        int actualLen = 0;
+        for(int i = 0; i < 26; i++){
+            if(ransomChar[i] != 0){
+                if(ransomChar[i] <= magazineChar[i]){
+                    actualLen++;
+                }
+            }
+        }
+        
+        if(actualLen == anslen){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
 //O(N) time and space both
 
 public boolean canConstruct(String ransomNote, String magazine) {
