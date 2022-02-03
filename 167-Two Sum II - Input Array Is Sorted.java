03a3da532b9d2cf9074bdbceb411329c0ac666b5
Sahil Patil <sahilpatil0922@gmail.com>
2022-02-03 12:37:58 +0530

    Update 167-Two Sum II - Input Array Is Sorted.java

--- 167-Two Sum II - Input Array Is Sorted.java
+++ 167-Two Sum II - Input Array Is Sorted.java
@@ -1,20 +1,23 @@
+// since array is sorted we can use 2 pointer approach
+// time complexity : O(N)
+
 public int[] twoSum(int[] numbers, int target) {
-        int[] ans = new int[2];
+        int[] res = new int[2];
         
         int i = 0;
         int j = numbers.length - 1;
- 
-        while(i<=j){   
-           if((numbers[i] + numbers[j]) > target){
-               j--;
-            }else if(numbers[i] + numbers[j] < target){
+        
+        while(i < j){
+            if(numbers[i] + numbers[j] < target){
                 i++;
+            }else if(numbers[i] + numbers[j] > target){
+                j--;
             }else{
-                ans[0] = i + 1; // since array is 1 - index based
-                ans[1] = j + 1;
+                res[0] = i + 1;
+                res[1] = j + 1;
                 break;
             }
         }
         
-        return ans;
+        return res;
     }
